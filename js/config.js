07fc7ef5404305b0d3fc427f71eb02807eb8d533
Yaroslav <yarik22k@gmail.com>
2023-11-28 19:24:13 +0100

    Amazon Cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_PM2ABT1wK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7hp6frbde54bs5v9es9qv07br8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
