72fd8651f063ca31198978ae63714f2dec78b592
yarik2kgus <107751198+yarik2kgus@users.noreply.github.com>
2023-11-29 09:51:12 +0100

    Update config.js for Cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_PM2ABT1wK', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7hp6frbde54bs5v9es9qv07br8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_pJC1XF29U', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '33rkutvct9mt1kdflkqurr4g9o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
